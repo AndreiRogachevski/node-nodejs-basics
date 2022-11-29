23b49f7545dda270a51b61802cd879c3b35e4cca
AndreiRogachevski <Arogov89@gmail.com>
2022-11-29 04:01:49 +0300

    mindBlow

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import { constants } from 'fs';
 const create = async () => {
-    // Write your code here 
+  try {
+    // try {
+    //   await fs.promises.access('src/fs/files/fresh.txt', constants.f_OK);
+    // } catch (error) {
+    //   console.error('FS operation failed');
+    // }
+    await fs.promises.writeFile(
+      'src/fs/files/fresh.txt',
+      'I am fresh and young',
+      function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await create();
